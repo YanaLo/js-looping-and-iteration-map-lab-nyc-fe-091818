81d8d4e29edd6686ec29fe9d8f254ea5a943d66d
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-08 20:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nameToAttributes(array){
 
 function attributesToPhrase(object){
   return object.map(driver=>{
-    return `${driver.name
+    return `${driver.name} is from ${driver.hometown}
   })
   
 }
