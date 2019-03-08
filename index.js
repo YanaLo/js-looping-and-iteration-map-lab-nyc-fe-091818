e87783c5e5e06230c60d6a7cacfc4313f294032f
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-08 20:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map((driver)=>{
+    return driver.toLowerCase()
+  })
+}
+
+function nameToAttributes(array){
+  return array.map(driver=>{
+  let firstDriver=driver.split(' ')[0]
+  let lastDriver=driver.split(' ')[1]
+  return {firstName:firstDriver, lastName:lastDriver}
+})
+}
